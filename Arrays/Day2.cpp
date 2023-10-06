f3a879804df766132e2b2f47bac36d55b42ad83d
Abhishek Gupta <116180843+abhic92@users.noreply.github.com>
2023-10-06 23:44:31 +0530

    Day2.cpp
    Today I completed 2 Question from Striver's DSA sheet                                  Ques 1 - Check Sorted Array                                                                         Ques 2 - Remove Duplicate from sorted array

--- Arrays/Day2.cpp
+++ Arrays/Day2.cpp
@@ -1,1 +1,29 @@
+Ques1. #Check if an Array is Sorted#
 
+solution:- 
+int isSorted(int n, vector<int> a) {
+   
+    for (int i = 0; i < n - 1; i++) {
+        if (a[i + 1] < a[i]) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+Ques2. #Remove Duplicates from Sorted Array#
+
+  solution:- class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=0;
+        for(int j=1; j<nums.size(); j++){
+            if(nums[i] != nums[j]){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+    }
+};
